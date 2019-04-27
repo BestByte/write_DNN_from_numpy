d7354c27e09eb119d322080cd722c115bbb7aedf
ilvxna <ilvxna@hotmail.com>
2019-04-27 21:12:20 +0800

    up

--- fullconeect.py
+++ fullconeect.py
@@ -1,2 +1,12 @@
 print(3)
 import numpy as np
+from activators import SigmoidActivator,IdentityActivator
+
+class FullConnectedLayer():
+    def __init__(self,input_size,output_size,activator):
+        '''
+        构造函数
+        input_size: 本层输入向量的维度
+        output_size: 本层输出向量的维度
+        activator: 激活函数
+        '''
